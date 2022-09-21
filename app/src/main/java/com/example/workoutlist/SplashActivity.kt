f95ced409168e1a3140682df6e9e5b819fb19cde
rohanmhetar <rohanvmhetar@gmail.com>
2022-09-21 15:38:35 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/workoutlist/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.workoutlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        final Intent i = new Intent(SplashActivity, MainActivity)
+    }
+}
